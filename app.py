26d53466229ae80ac192ca8e84238f2f1c1b6131
Kenneth Finnesand <kennethfinnesand@gmail.com>
2015-09-22 21:28:09 +0200

    Added run command to simplify starting of application

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 #!env/bin/python
 from app import app
-from flask.ext.script import Manager
-from flask_bootstrap import Bootstrap
+from flask.ext.script import Manager, Server
 
-bootstrap = Bootstrap(app)
 manager = Manager(app)
+
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
 
 if __name__ == '__main__':
     manager.run()
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+bootstrap = Bootstrap(app)
 
 from app.main import views
 from app.main import errors
