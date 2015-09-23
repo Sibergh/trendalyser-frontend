06c83ad1d035d82d1fe674378cb88b8da48c1c0f
Kenneth Finnesand <kennethfinnesand@gmail.com>
2015-09-23 11:39:43 +0200

    Added the ability to add keywords to database, and retrieves these keywords into a list. Also added new dependencies to requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 bootstrap = Bootstrap(app)
+db = MongoEngine(app)
 
 from app.main import views
 from app.main import errors
