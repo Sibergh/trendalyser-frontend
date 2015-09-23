a190c02f683e40a76591113e5aa3686d1e3febd4
Kenneth Finnesand <kennethfinnesand@gmail.com>
2015-09-23 12:36:53 +0200

    Added debug toolbar, and moved objects on the managmentpage

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+app.debug = True
+
 bootstrap = Bootstrap(app)
 db = MongoEngine(app)
+app.session_interface = MongoEngineSessionInterface(db)
+toolbar = DebugToolbarExtension(app)
 
 from app.main import views
 from app.main import errors
