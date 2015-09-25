bddafc0ff4c059ea0cf3a9131b021aec86ac114c
Kenneth Finnesand <kennethfinnesand@gmail.com>
2015-09-25 12:43:20 +0200

    Renamed app.py to manage.py, and added a new debug option to config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ SECRET_KEY = 'thisisasecret-key' #This needs to be changed in deployment
 
 MONGODB_SETTINGS = {'DB': 'trendalyser'}
 DEBUG_TB_PANELS = ['flask.ext.mongoengine.panels.MongoDebugPanel', 'flask_debugtoolbar.panels.timer.TimerDebugPanel', 'flask_debugtoolbar.panels.headers.HeaderDebugPanel', 'flask_debugtoolbar.panels.config_vars.ConfigVarsDebugPanel', 'flask_debugtoolbar.panels.template.TemplateDebugPanel', 'flask_debugtoolbar.panels.logger.LoggingPanel']
+DEBUG_TB_INTERCEPT_REDIRECTS = False
