117caa2f32d8bb29975bacf9b7c1f157212c4fda
Kenneth Finnesand <kennethfinnesand@gmail.com>
2015-09-25 12:46:04 +0200

    Changes in routing to work with blueprints

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from app import app, db
+from . import main
+from .. import db
 from .forms import *
 from .models import Keyword
 
-@app.route('/')
+@main.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/managekeywords', methods=['GET', 'POST'])
+@main.route('/managekeywords', methods=['GET', 'POST'])
 def managekeywords():
         form = addKeywordForm()
         if form.validate_on_submit():
@@ -15,7 +16,7 @@ def managekeywords():
             newkeyword.save()
             flash('New keyword added')
             form.keyword.data = ''
-            return redirect(url_for('managekeywords'))
+            return redirect(url_for('.managekeywords'))
         return render_template('managekeywords.html',
                                 form=form, wordlist=getkeywords())
 
